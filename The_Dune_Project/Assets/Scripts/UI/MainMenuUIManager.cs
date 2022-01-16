6a375cd26d1b8dbbedc082c5dd1aeee2317630af
Samantha Difeo <sdifeo1@gmail.com>
2022-01-16 14:55:17 -0500

    Audio save and load global settings

--- The_Dune_Project/Assets/Scripts/UI/MainMenuUIManager.cs
+++ The_Dune_Project/Assets/Scripts/UI/MainMenuUIManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -14,20 +16,19 @@ public class MainMenuUIManager : MonoBehaviour
     [SerializeField] private GameObject audioPage;
     [SerializeField] private GameObject controlsPage;
     private GameObject activeSettingsPage;
+    // an apply & reset to default buttons for settings?
+    
+    [Header("Audio")]
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider masterVolSlider;
+    [SerializeField] private Slider musicVolSlider;
+    [SerializeField] private Slider effectsVolSlider;
+    [SerializeField] private Slider uiEffectsVolSlider;
     
     private void Start()
     {
-        if (mainMenu == null || settingsMenu == null || visualsPage == null || audioPage == null || controlsPage == null)
-        {
-            Debug.LogError("No settings ui page in MainMenuUIManager");
-            return;
-        }
-        
-        settingsMenu.gameObject.SetActive(false);
-        visualsPage.SetActive(false);
-        audioPage.SetActive(false);
-        controlsPage.SetActive(false);
-        activeSettingsPage = null;
+        LoadAudio();
+        HideSettings();
     }
 
     private void Update()
@@ -71,6 +72,18 @@ public class MainMenuUIManager : MonoBehaviour
     
     /* Settings */
 
+    private void HideSettings()
+    {
+        Assert.IsTrue(mainMenu != null && settingsMenu != null && visualsPage != null && audioPage != null && controlsPage != null, 
+            "No settings ui page in MainMenuUIManager");
+
+        settingsMenu.gameObject.SetActive(false);
+        visualsPage.SetActive(false);
+        audioPage.SetActive(false);
+        controlsPage.SetActive(false);
+        activeSettingsPage = null;
+    }
+
     public void OnVisualsButtonClicked() => SetPageActive("visuals");
     
     public void OnAudioButtonClicked() => SetPageActive("audio");
@@ -86,7 +99,7 @@ public class MainMenuUIManager : MonoBehaviour
             Debug.LogError("No settings ui page in MainMenuUIManager");
             return;
         }
-        
+
         visualsPage.SetActive(false);
         audioPage.SetActive(false);
         controlsPage.SetActive(false);
@@ -97,7 +110,7 @@ public class MainMenuUIManager : MonoBehaviour
                 visualsPage.SetActive(true);
                 activeSettingsPage = visualsPage;
                 break;
-            
+
             case "audio":
                 audioPage.SetActive(true);
                 activeSettingsPage = audioPage;
@@ -114,6 +127,49 @@ public class MainMenuUIManager : MonoBehaviour
         }
     }
 
-    // an apply & reset to default buttons for settings?
+    /* Audio slides (set up with the Audio Mixer, which needs to be assigned to EVERY audio source in the game) */
+    private void LoadAudio()
+    {
+        Assert.IsTrue(masterVolSlider && musicVolSlider  && effectsVolSlider && uiEffectsVolSlider, 
+            "No audio slider in MainMenuUIManager");
+        
+        float masterVolLvl = PlayerPrefs.GetFloat("masterVolume", 0.75f);
+        float musicVolLvl = PlayerPrefs.GetFloat("musicVolume", 0.75f);
+        float effectsVolLvl = PlayerPrefs.GetFloat("effectsVolume", 0.50f);
+        float uiEffectsVolLvl = PlayerPrefs.GetFloat("uiEffectsVolume", 0.50f);
+        
+        masterVolSlider.value = masterVolLvl;
+        musicVolSlider.value = musicVolLvl;
+        effectsVolSlider.value = effectsVolLvl;
+        uiEffectsVolSlider.value = uiEffectsVolLvl;
+        
+        audioMixer.SetFloat ("masterVolume", Mathf.Log10(masterVolLvl) * 20);
+        audioMixer.SetFloat ("musicVolume", Mathf.Log10(musicVolLvl) * 20);
+        audioMixer.SetFloat ("effectsVolume", Mathf.Log10(effectsVolLvl) * 20);
+        audioMixer.SetFloat ("uiEffectsVolume", Mathf.Log10(uiEffectsVolLvl) * 20);
+    }
     
+    public void SetMasterVolumeLevel(float masterVolLvl)
+    {
+        audioMixer.SetFloat ("masterVolume", Mathf.Log10(masterVolLvl) * 20);
+        PlayerPrefs.SetFloat("masterVolume", masterVolLvl);
+    }
+
+    public void SetMusicVolumeLevel(float musicVolLvl)
+    {
+        audioMixer.SetFloat ("musicVolume", Mathf.Log10(musicVolLvl) * 20);
+        PlayerPrefs.SetFloat("musicVolume", musicVolLvl);
+    }
+    
+    public void SetEffectsVolumeLevel(float effectsVolLvl)
+    {
+        audioMixer.SetFloat ("effectsVolume", Mathf.Log10(effectsVolLvl) * 20);
+        PlayerPrefs.SetFloat("effectsVolume", effectsVolLvl);
+    }
+ 
+    public void SetUIEffectsVolumeLevel(float uiEffectsVolLvl)
+    {
+        audioMixer.SetFloat ("uiEffectsVolume", Mathf.Log10(uiEffectsVolLvl) * 20);
+        PlayerPrefs.SetFloat("uiEffectsVolume", uiEffectsVolLvl);
+    }
 }
