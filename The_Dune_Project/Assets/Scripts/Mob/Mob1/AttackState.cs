cc83fb26d0995637e7d2f9a690f7ae022d706df9
Steven <koreancoffe@gmail.com>
2022-02-10 11:11:53 -0500

    added enemy test model, added base state machine implementation

---
+++ The_Dune_Project/Assets/Scripts/Mob/Mob1/AttackState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class AttackState : BaseState
+{
+    //params
+    private float attackReadyTimer;
+    private Monster currEnemy;
+    
+
+    public AttackState(Monster Monster) : base(Monster.gameObject)
+    {
+        this.currEnemy = Monster;
+    }
+    
+    public override Type Action()
+    {
+        if (this.currEnemy.Target.Equals(null))
+        {
+            //go back to going around
+            return typeof(WanderState);
+        }
+        var distance = Vector3.Distance(this.transform.position, currEnemy.Target.transform.position);
+        if (distance >= 5f)
+        {
+            return typeof(ChaseState);
+        }
+        //else call function
+            this.currEnemy.Attack();
+            return null;
+        
+    }
+    
+
+}
