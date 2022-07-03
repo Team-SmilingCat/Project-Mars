71fc647881b1b923c9b662eae382d89d75358c39
Steven Lee <koreancoffe@gmail.com>
2022-07-02 21:02:09 -0400

    add

--- The_Dune_Project/Assets/Scripts/Runtime/Fight/GolemController.cs
+++ The_Dune_Project/Assets/Scripts/Runtime/Fight/GolemController.cs
@@ -13,12 +13,15 @@ public class GolemController : EnemyController
     [SerializeField] private PlayerManager playerManager;
     [SerializeField] private GlobalGameManager globalGameManager;
 
+    [SerializeField] private Vector3 newPos;
+
 
     private void Start()
     {
         LoadAgentProperties(this);
         enemyAnimatorManager = GetComponent<EnemyAnimatorManager>();
         enemyAnimatorManager.SetAnimBool("Awake", true);
+        newPos = transform.position;
     }
     
     void Update()
@@ -44,8 +47,29 @@ public class GolemController : EnemyController
     public void MoveToARandomWayPoint()
     {
         enemyAnimatorManager.SetAnimBool("walking", true);
-        Debug.Log("wef");
-        var newPos = potentialGoalPoints[UnityEngine.Random.RandomRange(0, potentialGoalPoints.Count)].position;
-        this.myAgent.SetDestination(newPos);
+        Debug.Log(Vector3.Distance(transform.position, newPos));
+        if (Vector3.Distance(transform.position, newPos) < 1.5f)
+        {
+            newPos = potentialGoalPoints[UnityEngine.Random.RandomRange(0, potentialGoalPoints.Count)].position;
+            this.myAgent.SetDestination(newPos);   
+        }
+    }
+
+    public void ShootSpears()
+    {
+        if (Vector3.Distance(transform.position, newPos) < 1.5f)
+        {
+            
+        }
+    }
+
+    public bool isCloseEnough()
+    {
+        if (Vector3.Distance(transform.position, target.position) < 1f)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
